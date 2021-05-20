ff5ae370f47b225fc11778678ccf7c7b3330729b
SuiPing <kam.suipingooi@me.com>
2021-05-20 10:24:31 +0000

    added stickers models

--- stickers/models.py
+++ stickers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# from pyuploadcare.dj.models import ImageField
+# from django import forms
 
 # Create your models here.
+
+
+class sticker(models.Model):
+    name = models.CharField(blank=False, max_length=255)
+    description = models.CharField(blank=False, max_length=255)
+    price = models.DecimalField(blank=False, max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
