30e2edd547db201d6a2bb011e7ba44b614090750
SuiPing <kam.suipingooi@me.com>
2021-06-12 04:00:26 +0000

    updated settings.py

--- TheMilkiiWay/settings.py
+++ TheMilkiiWay/settings.py
@@ -157,6 +157,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
