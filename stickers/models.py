bc39e6d199c07c64cbe609879014fc3260b0a181
SuiPing <kam.suipingooi@me.com>
2021-06-18 01:27:33 +0000

    updated validation for price in sticker model

--- stickers/models.py
+++ stickers/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from pyuploadcare.dj.models import ImageField
+from django.core.validators import MinValueValidator
+
 # from django import forms
 
 # Create your models here.
@@ -8,7 +10,8 @@ from pyuploadcare.dj.models import ImageField
 class sticker(models.Model):
     name = models.CharField(blank=False, max_length=255)
     description = models.CharField(blank=False, max_length=255)
-    price = models.DecimalField(blank=False, max_digits=5, decimal_places=2)
+    price = models.DecimalField(
+        blank=False, max_digits=5, decimal_places=2, validators=[MinValueValidator(1)])
     photo = ImageField(blank=False, manual_crop="")
 
     def __str__(self):
