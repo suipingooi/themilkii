de1561fed080f6531ab219e66478e775e8b44bcf
SuiPing <kam.suipingooi@me.com>
2021-06-19 08:42:08 +0000

    updated price validation

--- stickers/models.py
+++ stickers/models.py
@@ -7,11 +7,17 @@ from django.core.validators import MinValueValidator
 # Create your models here.
 
 
+def valiPrice(price):
+    if price > 0:
+        return float(price)
+
+
 class sticker(models.Model):
     name = models.CharField(blank=False, max_length=255)
     description = models.CharField(blank=False, max_length=255)
     price = models.DecimalField(
-        blank=False, max_digits=5, decimal_places=2, validators=[MinValueValidator(1)])
+        blank=False, max_digits=5, decimal_places=2,
+        validators=[MinValueValidator(1.00), valiPrice])
     photo = ImageField(blank=False, manual_crop="")
 
     def __str__(self):
